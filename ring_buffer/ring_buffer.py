1e0ce6bdd74fc5bdfeeb6e747d9c3502a998ffc4
HernanDarius <hdmecheverry@gmail.com>
2020-09-11 13:59:14 -0400

    fixed var

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,15 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = []
-        self.old_idx = 0
+        self.data = []
+        self.current = 0
 
     def append(self, item):
-        if len(self.storage) == self.capacity:
-            self.storage[self.old_idx] = item  #overriting if the storage is full
-            self.old_idx = self.old_idx+1 if self.old_idx+1 < len(self.storage) else 0
+        if len(self.data) == self.capacity:
+            self.data[self.current] = item  
+            self.current = self.current+1 if self.current+1 < len(self.data) else 0
         else:
-            self.storage.append(item)
+            self.data.append(item)
 
     def get(self):
-        return self.storage
+        return self.data
