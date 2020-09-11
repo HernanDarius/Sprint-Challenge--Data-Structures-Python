3aa600363030c7efa1681338912bd7eac77fbfee
HernanDarius <hdmecheverry@gmail.com>
2020-09-11 13:55:14 -0400

    finished sprint

--- names/names.py
+++ names/names.py
@@ -10,13 +10,18 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+all_names = [names_1, names_2]
+outer_loop = [set(name) for name in all_names]
+sym_un = outer_loop[0]
+sym_diff = outer_loop[0]
+
+for i in outer_loop[1:]:
+    sym_un = sym_un.union(i)
+    sym_diff = sym_diff.symmetric_difference(i)
+duplicates = list(sym_un.difference(sym_diff))
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
